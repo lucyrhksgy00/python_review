416e28eb2c8adf0bb0ffe507d6599da50191eb88
lucyrhksgy00 <lucyrhksgy00@gmail.com>
2022-08-29 14:10:59 +0900

    day01.py
    day01.py file

---
+++ day01.py
@@ -0,0 +1,93 @@
+# 산술 연산자
+1 + 1
+10 * 123
+
+10*2
+10**2
+2**10
+
+10/3
+10//3
+10%3
+
+
+# 대입 연산자
+num = 3
+num
+
+x = 100
+x
+
+
+# 복합대입연산자
+# 사용하지 않았을 때 (비효율적)
+total = 0
+total = total + 7000
+total = total + 3000
+total = total + 1200
+total
+point = total * 0.01
+point
+
+# 사용했을 때 (효율적)
+total = 0
+total += 7000
+total += 3000
+total += 1200
+total
+
+num = 10
+num-=2
+num
+
+
+# 비교연산자
+10 > 2
+-199 < -200
+0 >= -2
+-2 <= -2
+-2 == -2
+-2 != -2
+2 != 3
+
+
+# 논리연산자
+10 == 10
+not(10 == 10)
+
+# 자료형
+type(10)
+type(3.14)
+type('사과')
+type(True)
+
+# 동적 타이핑
+num = 3.14
+num
+type(num)
+
+num = '사과'
+num
+type(num)
+
+
+
+
+
+score1 = 10
+score2 = 23
+score3 = 98
+avgScore = (score1 + score2 + score3)/3
+avgScore
+avgScore = score1 + score2 + score3 / 3
+avgScore
+
+
+
+
+
+student1 = 123
+student2 = 331
+student3 = 85
+avg_student = (student1 + student2 + student3)/3
+avg_student
